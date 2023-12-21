4fc1a979f76981770f6dc1161c79faeee8b29ef3
Black Sparow <blacksparowyt@gmail.com>
2023-12-21 10:23:48 +0100

    v2.18.1 - SEOOOOOO
    More SEO bs, removed the sitemap generator and made sure the datatables stuff only loads on the needed page.
    
    Robots(.txt):
     - Removed some image stuff as it gets blocked for google
    
    Head:
     - Changed it so now the JS and CSS for datatables only loads with its required page.
    
    sitemapRequest:
     - Removed file as it doesnt seem to work

--- files/components/head.php
+++ files/components/head.php
@@ -22,8 +22,11 @@
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
-    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+
+    <?php if ($page['name'] == "projects" && $page['category'] == "account") { ?>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+        <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+    <?php } ?>
 
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-YBKBS0EKY7"></script>
     <script>
